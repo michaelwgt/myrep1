1fa3a3917e9530828018e9df82f8d273cc7ab525
michaelwgt <doskb8@163.com>
2017-10-05 23:02:17 -0700

    maven project 1

---
+++ src/main/java/com/javatpoint1/Consumer1App.java
@@ -0,0 +1,16 @@
+package com.javatpoint1;
+import com.javatpoint.*;
+/**
+ * Hello world!
+ *
+ */
+public class Consumer1App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from consumer!" );
+		App app=new App();
+		System.out.println( app.sayHello());
+		
+    }
+}
